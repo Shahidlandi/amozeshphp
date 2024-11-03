f83bc6756ade5cd0803842fae45bec1225127398
Honarestan Shaid Ali Landi <shahid.landi2024@gmail.com>
2024-11-03 10:27:25 +0330

    Add files via upload

---
+++ string-function.php
@@ -0,0 +1,45 @@
+<?php
+// $str = " ali reza ";
+// $len = strlen($str);
+// print $len;
+
+// $pos = strpos($str,"ali");
+// print $pos;
+
+// $sub = substr($str,0,-2);
+// print $sub;
+
+
+
+// print $str . "<br>";
+// print strlen($str) . "<br>";
+
+// $tr = trim($str);
+// print $tr . "<br>";
+// print strlen($tr);
+
+// $str = "my
+//         name";
+// print $str . "<br>";
+
+// $str1 = nl2br($str);
+// print $str1;
+
+
+
+// $str = "salam php 12 web";
+// print str_replace(' ', ';', $str);
+
+
+// $str = "12,15,20";
+// $str ="mohammad.ali.abass";
+// $a= explode('.', $str);
+// print_r($a);
+
+
+
+// $arr = [12.5,15,20,18,19];
+$arr = ['ali'=>20, 'mohammad'=>15, 'hassan'=>18];
+$a = implode('-',$arr);
+print $a;
+?>
